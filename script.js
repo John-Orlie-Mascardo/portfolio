cf08d74f3e29ae4fab212dcf6d342bea310f157e
John Orlie Mascardo <johnorliem@gmail.com>
2025-05-05 09:20:55 +0800

    Separating script

---
+++ script.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'bg-dark': '#22252d',
+            },
+            fontFamily: {
+                'slabo': ['"Slabo 27px"', 'serif'],
+            },
+        }
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    VANTA.WAVES({
+      el: "#ring",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x22252d,
+      shininess: 11.00,
+      waveHeight: 16.00,
+      waveSpeed: 1.10,
+      zoom: 0.76
+    })
+})
